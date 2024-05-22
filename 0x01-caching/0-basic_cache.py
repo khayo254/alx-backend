517e73a7cd8d050742634d8f2d3a6d60e5a30453
khayo254 <khayo1998@gmail.om>
2024-05-22 18:22:36 +0300

    add return messae

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,7 +12,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in cache"""
         if key is None and item is None:
-            self.cache_data[key] = item
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key"""
