bd2d4f6187591110b8c08b9e2c039291e52ab2cd
khayo254 <khayo1998@gmail.om>
2024-05-22 18:18:00 +0300

    logic change

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,21 +2,31 @@
 """
 Basic cache module
 """
-
-
 from base_caching import BaseCaching
+
 
 class BasicCache(BaseCaching):
     """
     BasicCache class that inherits from BaseCaching
     """
-    
     def put(self, key, item):
         """ Add an item in cache"""
-        if key is not None and item is not None:
+        if key is None and item is None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Get an item by key"""
         if key is not None:
-            return self.cache_data.get(key)
+            return self.cache_data.get(key, None)
+
+# Testing the BasicCache class
+if __name__ == "__main__":
+    my_cache = BasicCache()
+
+    my_cache.put("key1", "value1")
+    my_cache.put("key2", "value2")
+
+    my_cache.print_cache()
+
+    print(my_cache.get("key1"))
+    print(my_cache.get("key3"))
