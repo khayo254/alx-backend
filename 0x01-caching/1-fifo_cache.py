04c7d17dc965e6f756adc21b523b51c8dbde6dd3
khayo254 <khayo1998@gmail.om>
2024-05-22 19:34:12 +0300

    first come! first served!

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" FIFOCache module
+"""
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initialize FIFOCache instance
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache with FIFO eviction strategy
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key = self.queue.pop(0)
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data
