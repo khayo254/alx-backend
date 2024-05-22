1a2f54068c15bd75c10dbf7dee45a854f3ee7652
khayo254 <khayo1998@gmail.om>
2024-05-22 18:13:39 +0300

    removed return

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,8 +14,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in cache"""
         if key is not None and item is not None:
-            return
-        self.cache_data[key] = item
+            self.cache_data[key] = item
     
     def get(self, key):
         """ Get an item by key"""
